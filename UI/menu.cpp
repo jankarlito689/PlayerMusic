8f228588aa4e20cbf58e9350cbcc451f94566bb6
cesar jan karlo <leossuarez103@gmail.com>
2025-11-20 12:42:52 -0600

    se agrego ui a playlist

--- UI/menu.cpp
+++ UI/menu.cpp
@@ -1,4 +1,6 @@
 #include "menu.hpp"
+#include "../Utils/utils.hpp"
+#include <iomanip>
 
 // Colores ANSI
 #define CYAN "\033[1;36m"
@@ -12,7 +14,7 @@ namespace Ui {
 
 void drawHeader() {
     cout << CYAN << "╔════════════════════════════════════════════════════════════╗\n";
-    cout << "║" << WHITE << "                🎧  REPRODUCTOR MUSICAL C+     " << CYAN << "║\n";
+    cout << "║" << WHITE << "                🎧  REPRODUCTOR MUSICAL C+     "       << CYAN <<             "             ║\n";
     cout << "╠════════════════════════════════════════════════════════════╣\n";
     cout << RESET;
 }
@@ -45,7 +47,7 @@ void drawProgressBar(float current, float total) {
          << currentInt / 60 << ":" << (currentInt % 60 < 10 ? "0" : "") << currentInt % 60 
          << WHITE << "  /  " << GREEN
          << totalInt / 60 << ":" << (totalInt % 60 < 10 ? "0" : "") << totalInt % 60
-         << RESET << "                                  ║\n";
+         << RESET << "                            ║\n";
 
     cout << GREEN << "╚══════════════════════════════════════════╝\n" << RESET;
 }
@@ -53,27 +55,27 @@ void drawProgressBar(float current, float total) {
 void drawMenu() {
     drawHeader();
     cout << WHITE
-        << "║ " << YELLOW << "1) " << WHITE << "Pausar canción                              ║\n"
-        << "║ " << YELLOW << "2) " << WHITE << "Reanudar canción                            ║\n"
-        << "║ " << YELLOW << "3) " << WHITE << "Siguiente canción                           ║\n"
-        << "║ " << YELLOW << "4) " << WHITE << "Canción anterior                            ║\n"
-        << "║ " << YELLOW << "5) " << WHITE << "Cambiar de playList                         ║\n"
-        << "║ " << YELLOW << "6) " << WHITE << "Gestión de playList                         ║\n"
-        << "║ " << YELLOW << "0) " << WHITE << "Salir del reproductor                       ║\n";
+        << "║ " << YELLOW << "1) " << WHITE << "Pausar canción                                          ║\n"
+        << "║ " << YELLOW << "2) " << WHITE << "Reanudar canción                                        ║\n"
+        << "║ " << YELLOW << "3) " << WHITE << "Siguiente canción                                       ║\n"
+        << "║ " << YELLOW << "4) " << WHITE << "Canción anterior                                        ║\n"
+        << "║ " << YELLOW << "5) " << WHITE << "Cambiar de playList                                     ║\n"
+        << "║ " << YELLOW << "6) " << WHITE << "Gestión de playList                                     ║\n"
+        << "║ " << YELLOW << "0) " << WHITE << "Salir del reproductor                                   ║\n";
     drawFooter();
 }
 
 void drawPlayListMenu() {
     drawHeader();
     cout << WHITE
-        << "║ " << YELLOW << "1) " << WHITE << "Registrar nueva canción                    ║\n"
-        << "║ " << YELLOW << "2) " << WHITE << "Buscar canción                              ║\n"
-        << "║ " << YELLOW << "3) " << WHITE << "Ordenar canciones                           ║\n"
-        << "║ " << YELLOW << "4) " << WHITE << "Invertir lista                              ║\n"
-        << "║ " << YELLOW << "5) " << WHITE << "Editar canción                              ║\n"
-        << "║ " << YELLOW << "6) " << WHITE << "Eliminar canción                            ║\n"
-        << "║ " << YELLOW << "7) " << WHITE << "Vaciar lista                                ║\n"
-        << "║ " << YELLOW << "0) " << WHITE << "Volver al menú principal                    ║\n";
+        << "║ " << YELLOW << "1) " << WHITE << "Registrar nueva canción                                 ║\n"
+        << "║ " << YELLOW << "2) " << WHITE << "Buscar canción                                          ║\n"
+        << "║ " << YELLOW << "3) " << WHITE << "Ordenar canciones                                       ║\n"
+        << "║ " << YELLOW << "4) " << WHITE << "Invertir lista                                          ║\n"
+        << "║ " << YELLOW << "5) " << WHITE << "Editar canción                                          ║\n"
+        << "║ " << YELLOW << "6) " << WHITE << "Eliminar canción                                        ║\n"
+        << "║ " << YELLOW << "7) " << WHITE << "Vaciar lista                                            ║\n"
+        << "║ " << YELLOW << "0) " << WHITE << "Volver al menú principal                                ║\n";
     drawFooter();
 }
 
@@ -85,4 +87,79 @@ void showPlayingInfo(const string &name, const string &artist, const string &dur
     cout << GREEN << "╚════════════════════════════════════════════════════════════╝\n" << RESET;
 }
 
+void drawPlaylist(const CircularList &playlist, Node *currentPlaying) {
+    saveCursor();   // Guardar posición original
+
+    int X = 75;  
+    int Y = 2;
+
+    gotoxy(X, Y);
+    cout << CYAN << "╔══════════════════════════════════════════════════════════════════════════╗";
+
+    gotoxy(X, Y + 1);
+    cout << "║ " << WHITE << "PLAYLIST ACTUAL" << CYAN
+         << "                                                          ║";
+
+    gotoxy(X, Y + 2);
+    cout << "╠══════════════════════════════════════════════════════════════════════════╣";
+
+    // Encabezados
+    gotoxy(X, Y + 3);
+    cout << WHITE 
+         << "║ " << CYAN << setw(6) << left << "Índice"
+         << WHITE << "│ " << CYAN << setw(25) << left << "Nombre Canción"
+         << WHITE << "│ " << CYAN << setw(20) << left << "Artista"
+         << WHITE << "│ " << CYAN << setw(8)  << left << "Duración" << WHITE
+         << " ║";
+
+    gotoxy(X, Y + 4);
+    cout << CYAN << "╠══════════════════════════════════════════════════════════════════════════╣" 
+         << RESET;
+
+    Node* temp = playlist.getHead();
+
+    int row = 5;
+
+    if (!temp) {
+        gotoxy(X, Y + row);
+        cout << "║ (Lista vacía)                                                             ║";
+
+        gotoxy(X, Y + row + 1);
+        cout << CYAN << "╚══════════════════════════════════════════════════════════════════════════╝" 
+             << RESET;
+
+        restoreCursor();
+        return;
+    }
+    do {
+        gotoxy(X, Y + row);
+
+        bool isPlaying = (temp == currentPlaying);
+
+        if (isPlaying) {
+            cout << BLUE << "║ "
+                 << WHITE << setw(6) << left << temp->song.id
+                 << BLUE << "│ " << WHITE << setw(25) << left << temp->song.name
+                 << BLUE << "│ " << WHITE << setw(20) << left << temp->song.artist
+                 << BLUE << "│ " << WHITE << setw(8)  << left << temp->song.duration
+                 << BLUE << " ║" << RESET;
+        } else {
+            cout << "║ "
+                 << WHITE << setw(6) << left << temp->song.id
+                 << WHITE << "│ " << setw(25) << left << temp->song.name
+                 << "│ " << setw(20) << left << temp->song.artist
+                 << "│ " << setw(8)  << left << temp->song.duration
+                 << " ║";
+        }
+
+        temp = temp->next;
+        row++;
+
+    } while (temp != playlist.getHead());
+
+    gotoxy(X, Y + row);
+    cout << CYAN << "╚══════════════════════════════════════════════════════════════════════════╝"
+        << RESET;
+    restoreCursor();  // ← ESTE SÍ VA AQUÍ (solo una vez)
+}
 } // namespace Ui
